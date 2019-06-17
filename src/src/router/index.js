d5d04c6aed7083621dc972a2553da7d7b81f2c2f
Wangzihao5325 <416990273@qq.com>
2019-06-17 11:26:31 +0700

    1.以index作为唯一的信息

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -5,6 +5,7 @@ import Variables from '../global/Variables';
 
 import ResponsiveHeader from '../component/header/index';
 
+const KEY_ARR = Object.keys(Variables.menuKeyMapToRouter);
 const MainScreen = asyncImport(() => import('../screen/main/index'));
 const ShowList = asyncImport(() => import('../screen/show_list/index'));
 const AboutMe = asyncImport(() => import('../screen/about_me/index'));
@@ -16,9 +17,9 @@ export default class AppRouter extends Component {
                 <div>
                     <ResponsiveHeader />
                     <Switch>
-                        <Route path={Variables.menuKeyMapToRouter['blog']} exact component={MainScreen} />
-                        <Route path={Variables.menuKeyMapToRouter['show-list']} exact component={ShowList} />
-                        <Route path={Variables.menuKeyMapToRouter['about-me']} exact component={AboutMe} />
+                        <Route path={Variables.menuKeyMapToRouter[KEY_ARR[0]]} exact component={MainScreen} />
+                        <Route path={Variables.menuKeyMapToRouter[KEY_ARR[1]]} exact component={ShowList} />
+                        <Route path={Variables.menuKeyMapToRouter[KEY_ARR[2]]} exact component={AboutMe} />
                     </Switch>
                 </div>
             </Router>
